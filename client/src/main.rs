43f193965c3b1a5144ace2067acd38b58151f7a6
Ollo <ollo@ollo.mobil>
2023-05-17 21:15:26 +0200

    Set more pixel in client

--- client/src/main.rs
+++ client/src/main.rs
@@ -6,8 +6,16 @@ const IMAGE_HEIGHT_BYTE : usize = 40;
 
 fn send_package(ipaddress: String) {
     let mut image = [0; (IMAGE_WIDTH_BYTE*IMAGE_HEIGHT_BYTE)+1];
-    image[0] = 0xFF;
-    image[1] = 0xFF;
+    // Brightnes
+    image[0] = 0x80;
+    // row 0, led 0
+    image[1] = 1;
+    // row 0, led 8
+    image[2] = 1;
+    // row 0, led 16 and 17
+    image[3] = 3;
+    // row 0, led 24, 25, 26
+    image[4] = 7;
     let socket = UdpSocket::bind("0.0.0.0:4242").expect("couldn't bind to address");
     socket.send_to(&image, ipaddress + ":4242").expect("couldn't send data");
     println!("Packet sent");
