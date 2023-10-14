f19c6738767ec15ddab44d586e7b561b0a5cd69b
Ada <git@jooa.xyz>
2023-10-14 13:10:38 +0200

    Refactor code somewhat

--- client/bin/src/straba.rs
+++ client/bin/src/straba.rs
@@ -68,6 +68,20 @@ pub struct IsoStringDateTime {
     pub iso_string: Option<String>,
 }
 
+#[derive(Debug)]
+enum APIError {
+    Request(reqwest::Error),
+    Read(reqwest::Error),
+    Parse(serde_json::Error),
+}
+
+fn api_request(url: &String) -> Result<Station, APIError> {
+    let body = reqwest::blocking::get(url).map_err(APIError::Request)?;
+    let text = body.text().map_err(APIError::Read)?;
+    let result = serde_json::from_str(&text).map_err(APIError::Parse);
+    result
+}
+
 // Return value
 pub struct NextDeparture {
     pub request_time: i64,
@@ -78,82 +92,64 @@ pub struct NextDeparture {
     pub failure: bool,
 }
 
-pub fn fetch_data(debug_print : Option<bool>) -> NextDeparture {
-
-    let st_now = SystemTime::now();
-    let seconds = st_now.duration_since(UNIX_EPOCH).unwrap().as_secs();
-    let url = &format!("{}?datetime={}", STATION_URL, seconds);
-    let result = reqwest::blocking::get(url);
-    
-    let mut return_value = NextDeparture {
-        failure  : false,
-        outbound_station  : String::from(""),
-        outbound_diff : 10000,
-        inbound_station : String::from(""),
-        inbound_diff : 10000,
-        request_time : seconds as i64,
-    };
-
-    if result.is_err() {
-        println!("Could not read station response {:?}", result.err());
-        return_value.failure = true;
-        return return_value;
-    }
-    let text = result.unwrap().text();
-    if text.is_err() {
-        println!("Could not convert response {:?}", text.err());
-        return_value.failure = true;
-        return return_value;
-    }
-
-    let raw_text = &text.unwrap();
-    let body: std::result::Result<Station, serde_json::Error> = serde_json::from_str(&raw_text);
-
-    if body.is_err() {
-        println!("Could not parse json {:?}", body.err());  
-        println!("------------------------- %< ----------------------------");
-        println!("{}", &raw_text);
-        println!("------------------------- %< ----------------------------");
-        return_value.failure = true;
-        return return_value;
-    }
-
-    // parse JSON result.. search of both directions
-    let json = body.unwrap();
-    for el in json.graph_ql.response.journeys.elements {
-        if debug_print.is_some() && debug_print.unwrap() == true {
-            println!("Line {:}", el.line.line_group.label);  
+impl NextDeparture {
+    fn new(seconds: i64) -> NextDeparture {
+        NextDeparture {
+            failure  : false,
+            outbound_station  : String::from("Error"),
+            outbound_diff : 666*60,
+            inbound_station : String::from("Error"),
+            inbound_diff : 666*60,
+            request_time : seconds,
         }
-        for stop in el.stops {
-            // use only valid data
-            if stop.realtime_departure.iso_string.is_some() && 
-                stop.destination_label != "" {
-                let txt_departure = stop.realtime_departure.iso_string.unwrap();
-                let next_departure = DateTime::parse_from_rfc3339(&txt_departure).unwrap();
-                
-                let diff = next_departure.timestamp() - (seconds  as i64);
-                if debug_print.is_some() && debug_print.unwrap() == true {
-                    println!("To      {:} {:} (in {:} seconds)", stop.destination_label, txt_departure, diff );
-                }
-                
-                if stop.destination_label.contains("Rheinau") {
-                    if diff <  return_value.outbound_diff {
-                        return_value.outbound_station = stop.destination_label;
-                        return_value.outbound_diff = diff;
+    }
+    fn populate_departure(&mut self, result: Station, debug_print: Option<bool>) {
+        let journeys = result.graph_ql.response.journeys.elements;
+        if debug_print.is_some() && debug_print.unwrap() == true {
+            println!("Found {} journeys", journeys.len());
+        }
+        for el in journeys {
+            for stop in el.stops {
+                // use only valid data
+                if stop.realtime_departure.iso_string.is_some() && 
+                    stop.destination_label != "" {
+                    let txt_departure = stop.realtime_departure.iso_string.unwrap();
+                    let next_departure = DateTime::parse_from_rfc3339(&txt_departure).unwrap();
+                    
+                    let diff = next_departure.timestamp() - (self.request_time  as i64);
+                    if debug_print.is_some() && debug_print.unwrap() == true {
+                        println!("To      {:} {:} (in {:} seconds)", stop.destination_label, txt_departure, diff );
                     }
-                } else if stop.destination_label.contains("Hochschule") ||
-                            stop.destination_label.contains("Hauptbahnhof") ||
-                            stop.destination_label.contains("Schönau") {
-                    if diff <  return_value.inbound_diff {
-                        return_value.inbound_station = stop.destination_label;
-                        return_value.inbound_diff = diff;
+                    
+                    if stop.destination_label.contains("Rheinau") {
+                        if diff <  self.outbound_diff {
+                            self.outbound_station = stop.destination_label;
+                            self.outbound_diff = diff;
+                        }
+                    } else if stop.destination_label.contains("Hochschule") ||
+                                stop.destination_label.contains("Hauptbahnhof") ||
+                                stop.destination_label.contains("Schönau") {
+                        if diff <  self.inbound_diff {
+                            self.inbound_station = stop.destination_label;
+                            self.inbound_diff = diff;
+                        }
                     }
+                } else {
+                    println!("Planned {:} {:?}", stop.destination_label, stop.planned_departure.iso_string)
                 }
-            } else {
-                println!("Planned {:} {:?}", stop.destination_label, stop.planned_departure.iso_string)
             }
         }
     }
+}
 
-    return_value
+pub fn fetch_data(debug_print : Option<bool>) -> NextDeparture {
+    let st_now = SystemTime::now();
+    let seconds = st_now.duration_since(UNIX_EPOCH).unwrap().as_secs();
+    let url = &format!("{}?datetime={}", STATION_URL, seconds);
+    let mut return_value = NextDeparture::new(seconds as i64);
+    match api_request(url) {
+        Ok(content) => return_value.populate_departure(content, debug_print),
+        Err(error) => eprintln!("Error! {:?}", error)
+    };
+    return return_value
 }
