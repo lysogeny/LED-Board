f0d8c6aae5fb9c9cb9f51707f8e9658d5725214a
Ada <git@jooa.xyz>
2023-10-14 20:11:04 +0200

    Remove superfluous code, add initial text

--- rust-simulation/src/main.rs
+++ rust-simulation/src/main.rs
@@ -1,29 +1,23 @@
-use log;
-use env_logger;
-use std::{thread, time::Duration};
-use std::net::UdpSocket;
 use embedded_graphics::{
-    pixelcolor::BinaryColor,
-    mono_font::{ascii::FONT_10X20, MonoTextStyle},
-    text::Text,
     image::{Image, ImageRaw},
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::BinaryColor,
     prelude::*,
+    text::{Alignment, Baseline, Text, TextStyleBuilder},
 };
 use embedded_graphics_simulator::{
     BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
 };
+use env_logger;
+use log;
+use std::net::UdpSocket;
 
-const IMAGE_SIZE_BYTE: usize = (IMAGE_WIDTH_BYTE * IMAGE_HEIGHT) as usize; // one byte contains 8 LEDs, one in each bit
-const PANEL_WIDTH: u32 = 32 ;
-const PANEL_WIDTH_BYTE: u32 = PANEL_WIDTH / 8; // one byte contains 8 LEDs, one in each bit
-const PANEL_COUNT: u32 = 5 ;
-const PANEL_PIXELS: u32 = PANEL_WIDTH * 40;
-const PANEL_BYTES: u32 = PANEL_PIXELS / 8; // one byte contains 8 LEDs, one in each bit
+const PANEL_WIDTH: u32 = 32;
+const PANEL_COUNT: u32 = 5;
 const IMAGE_WIDTH: u32 = PANEL_COUNT * PANEL_WIDTH;
-const IMAGE_WIDTH_BYTE: u32 = IMAGE_WIDTH / 8; // one byte contains 8 LEDs, one in each bit
 const IMAGE_HEIGHT: u32 = 40;
-const IMAGE_HEIGHT_BYTE: u32 = 40;
-const IMAGE_LENGTH: usize = (IMAGE_WIDTH_BYTE * IMAGE_HEIGHT_BYTE) as usize;
+const IMAGE_WIDTH_BYTE: u32 = IMAGE_WIDTH / 8;
+const IMAGE_LENGTH: usize = (IMAGE_WIDTH_BYTE * IMAGE_HEIGHT) as usize;
 const PACKAGE_LENGTH: usize = (IMAGE_LENGTH + 1) as usize;
 
 struct Display {
@@ -37,8 +31,8 @@ impl Display {
             .theme(BinaryColorTheme::OledBlue)
             .scale(4)
             .build();
-        Display{
-            display: SimulatorDisplay::new(Size::new(32*5, 40)),
+        Display {
+            display: SimulatorDisplay::new(Size::new(32 * 5, 40)),
             window: Window::new("LED Board", &output_settings),
         }
     }
@@ -54,10 +48,8 @@ impl Display {
 }
 
 struct Client {
-    addr: String, 
-    port: i32,
     socket: UdpSocket,
-    display: Display
+    display: Display,
 }
 
 type ImgData = [u8; IMAGE_LENGTH];
@@ -67,11 +59,9 @@ impl Client {
         let addr_full = format!("{addr}:{port}");
         log::info!("Listening on {addr_full}");
         let socket = UdpSocket::bind(addr_full).expect("Couldn't bind");
-        Client{
-            addr: addr.to_string(), 
-            port,
+        Client {
             socket,
-            display: Display::new()
+            display: Display::new(),
         }
     }
     fn recv_image(&mut self) -> ImgData {
@@ -87,30 +77,47 @@ impl Client {
                         data[i] = data[i].reverse_bits();
                     }
                     return data.try_into().unwrap();
-                },
+                }
                 Ok((bytes_read, src_addr)) => {
                     log::info!("Responding to status checking message from {}", src_addr);
                     let data = &mut data[..bytes_read];
                     match self.socket.send_to(&data, &src_addr) {
-                        Ok(_) => {},
+                        Ok(_) => {}
                         Err(e) => log::error!("{:}", e),
                     };
                 }
             }
         }
     }
-    fn run(&mut self) -> Result<(), ()> {
+    fn draw_info(&mut self) {
+        let content = format!("Listening on {:}", self.socket.local_addr().unwrap());
+        let text_style = TextStyleBuilder::new()
+            .baseline(Baseline::Middle)
+            .alignment(Alignment::Center)
+            .build();
+        let char_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
+        Text::with_text_style(
+            &content,
+            self.display.display.bounding_box().center(),
+            char_style,
+            text_style,
+        )
+        .draw(&mut self.display.display)
+        .unwrap();
+    }
+    fn run(&mut self) {
         self.display.clear();
+        self.draw_info();
         self.display.update();
         'running: loop {
             self.display.clear();
             let data = self.recv_image();
             let image_raw = ImageRaw::<BinaryColor>::new(&data, IMAGE_WIDTH);
             let image = Image::new(&image_raw, Point::zero());
-                image.draw(&mut self.display.display).unwrap();
+            image.draw(&mut self.display.display).unwrap();
             self.display.update();
             if self.display.has_quit() {
-                break 'running Ok(());
+                break 'running;
             }
         }
     }
@@ -119,12 +126,5 @@ impl Client {
 fn main() {
     env_logger::init();
     let mut client = Client::new("0.0.0.0", 4242);
-    client.run().unwrap();
-    /*
-    let mut simulator = Simulator::new();
-    match simulator.run() {
-        Ok(v) => println!("Graceful exit {}", v), 
-        Err(_) => eprintln!("Failed!"),
-    }
-    */
+    client.run();
 }
